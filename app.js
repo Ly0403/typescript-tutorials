8f3464babd6ec74f8d532b8ce27ed7c769217a58
Ly0403 <yucel5585@gmail.com>
2023-03-22 19:45:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var msg = "Hello World3";
+var heading = document.createElement("h1");
+heading.textContent = msg;
+document.body.appendChild(heading);
+var number1;
+number1 = "";
+var newArray = [1, 2, 3, 4]; // number array
+var stringArray = ['adfd', 'dfsdf']; // string array
+var newObject;
+console.log(newObject.name);
---
+++ app.ts
@@ -0,0 +1,17 @@
+let msg:string ="Hello World3";
+
+let heading=document.createElement("h1");
+heading.textContent=msg;
+document.body.appendChild(heading);
+
+let number1:string;
+number1="";
+
+let newArray:number[]=[1,2,3,4];// number array
+let stringArray:string[]=['adfd','dfsdf'] // string array
+let newObject:{
+    name:string,
+    age:number
+}
+
+console.log(newObject.name);
